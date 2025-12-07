12d26dae1c99725ee148142ae3a9f8cd61e920a8
adem-loghmari <152447544+adem-loghmari@users.noreply.github.com>
2025-12-07 01:03:48 +0100

    Update image URL format in uploadImage function

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -51,7 +51,7 @@ const deleteProduct = async (req, res) => {
 const uploadImage = async (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:${PORT}/images/${req.file.filename}`,
+    image_url: `/images/${req.file.filename}`,
   });
 };
 
